8bd7972c6691d4c497b259350697a2afe34bda24
Sukanya <106397577+Gund008@users.noreply.github.com>
2024-08-18 16:55:22 +0530

    Add files via upload

---
+++ Enum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+  // Create an enum variable and assign a value to it
+  enum Level myVar = MEDIUM;
+
+  // Print the enum variable
+  printf("%d", myVar);
+
+  return 0;
+}
